4e0f8b1007d316ff03c775cc1f659dcfeb96db83
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-04-08 18:04:06 +0900

    조금씩 수정

--- 005/005/main.cpp
+++ 005/005/main.cpp
@@ -1,41 +1,41 @@
 #include<iostream>
 using namespace std;
 
-int a = 0;
-int c = 0;
-void func(int n[])
-{
-	if (n[c] > a&& c < 5)
-	{
-		a = n[c];
-		c++;
-		func(n);
-	}
-	else if(c < 5)
-	{
-		c++;
-		func(n);
-	}
-	else
-	{
-
-	}
-
-}
-int main()
-{
-//	cout << "입력 받을 숫자의 개수 : ";
-//	cin >> b;
-	int asd[5] = { 0, };
-	for (int i = 0; i < 5; i++)
-	{
-		cin >> asd[i];
-		
-	}
-		func(asd);
-	cout << a;
-	return(0);
-}
+//int a = 0;
+//int c = 0;
+//void func(int n[])
+//{
+//	if (n[c] > a&& c < 5)
+//	{
+//		a = n[c];
+//		c++;
+//		func(n);
+//	}
+//	else if(c < 5)
+//	{
+//		c++;
+//		func(n);
+//	}
+//	else
+//	{
+//
+//	}
+//
+//}
+//int main()
+//{
+////	cout << "입력 받을 숫자의 개수 : ";
+////	cin >> b;
+//	int asd[5] = { 0, };
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cin >> asd[i];
+//		
+//	}
+//		func(asd);
+//	cout << a;
+//	return(0);
+//}
 
 //int temp = 0;
 //int A = 0;
@@ -68,27 +68,27 @@ int main()
 //	return(0);
 //}
 
-//int a = 1;
-//void func(int n)
-//{
-//	cout <<n<<" x "<<a<<" = "<< n * a<<endl;
-//	if (a >= 9)
-//	{
-//		a = 1;;
-//	}
-//	else
-//	{
-//		a++;
-//		func(n);
-//	}
-//}
-//int main()
-//{
-//	int b;
-//	cin >> b;
-//
-//	func(b);
-//
-//	return(0);
-//}
-//
+int a = 1;
+void func(int n)
+{
+	cout <<n<<" x "<<a<<" = "<< n * a<<endl;
+	if (a >= 9)
+	{
+		a = 1;;
+	}
+	else
+	{
+		a++;
+		func(n);
+	}
+}
+int main()
+{
+	int b;
+	cin >> b;
+
+	func(b);
+
+	return(0);
+}
+
